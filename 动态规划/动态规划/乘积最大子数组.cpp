19b7dbd75b62b68e9069a94f6ce5dfc8e4fdc8e3
SiGH3 <liyou010307@gmail.com>
2025-02-21 15:54:17 +0800

    2.21打卡

--- 动态规划/动态规划/乘积最大子数组.cpp
+++ 动态规划/动态规划/乘积最大子数组.cpp
@@ -7,16 +7,16 @@ class Solution{
 public:
     int maxProduct(vector<int>& nums){
         int n = nums.size();
-        int res
+        int res = nums[0];
         vector<int> dpMin(n+1);
         dpMin[0]=nums[0];
         vector<int> dpMax(n+1);
         dpMax[0]=nums[0];
         for(int i = 1;i<nums.size();i++){
-           
-            dpMin[i]=max(nums[i],dp[i-1]*nums[i]);   
+            dpMax[i]=max(dpMax[i-1]*nums[i],max(nums[i],dpMin[i-1]*nums[i]));
+            dpMin[i]=min(dpMin[i-1]*nums[i],min(nums[i],dpMax[i-1]*nums[i]));   
+            res = max(res,dpMax[i]);
         }
-
+        return res;
     }
-
-}
+};
--- 动态规划/动态规划/单词拆分.cpp
+++ 动态规划/动态规划/单词拆分.cpp
@@ -3,12 +3,25 @@
 
 using namespace std;
 
-class Solution 
-{
-public:
-    bool wordBreak(string s, vector<string>& wordDict) 
-    {
-        int n = s.length();
-        vector <bool> dp(n + 1,false);
-    }
-};
+class Solution {
+    public:
+        //如果求组合数就是外层for循环遍历物品，内层for遍历背包。
+        //如果求排列数就是外层for遍历背包，内层for循环遍历物品。
+        //本题求排列
+        bool wordBreak(string s, vector<string>& wordDict) {
+            unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
+            
+            //dp[i] : 字符串长度为i的话，dp[i]为true，表示可以拆分为一个或多个在字典中出现的单词。
+            vector<bool> dp(s.size() + 1, false);
+            dp[0] = true;
+            for (int i = 1; i <= s.size(); i++) {   //遍历背包
+                for (int j = 0; j < i; j++) {       //遍历物品
+                    string word = s.substr(j, i - j);
+                    if (wordSet.find(word) != wordSet.end() && dp[j]) {
+                        dp[i] = true;
+                    }
+                }
+            }
+            return dp[s.size()];
+        }
+    };
