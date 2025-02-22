83066490c35f4a7e18d0fd2027b6f8b46e66f096
SiGH3 <liyou010307@gmail.com>
2025-02-22 18:39:57 +0800

    2.22打卡

--- 动态规划/动态规划/分割等和子集.cpp
+++ 动态规划/动态规划/分割等和子集.cpp
@@ -6,7 +6,25 @@ using namespace std;
 class Solution{
 public:
     bool canPartition(vector<int>& nums){
-        
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+        }
+        if(sum%2!=0){
+            return false;
+        }
+        int target = sum/2;
+        vector<int> dp(target+1,0);
+        for(int i=0;i<nums.size();i++){
+            for(int j=target;j>=nums[i];j--){
+                dp[j]=max(dp[j],dp[j-nums[i]]+nums[i]);
+            }
+        }
+        if(dp[target]==target){
+            return true;
+        }else{
+            return false;
+        }
     }
 
 };
