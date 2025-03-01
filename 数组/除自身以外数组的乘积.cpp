16e6cf4d708bc48c5babda9a1ad7c67e61e6501a
SiGH3 <liyou010307@gmail.com>
2025-03-01 10:47:24 +0800

    2.28

--- 数组/除自身以外数组的乘积.cpp
+++ 数组/除自身以外数组的乘积.cpp
@@ -8,13 +8,18 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
         vector<int> res(n);
-        vector<int> left(n,1);
-        vector<int> right(n,1);
+        vector<int> left(n);
+        vector<int> right(n);
+        left[0]=1;
         for(int i=1;i<n;i++){   //left[i]表示nums[i]左边数字的乘积
             left[i] = left[i-1]*nums[i-1];
         }
-        for(int i=n-2;i>1;i--){  //right[i]表示nums[i]右边数字的乘积
+        right[n-1]=1;
+        for(int i=n-2;i>=0;i--){  //right[i]表示nums[i]右边数字的乘积
             right[i] = right[i+1]*nums[i+1];
+        }
+        for(int i=0;i<n;i++){
+            res[i]=left[i]*right[i];
         }
         return res;
     }
